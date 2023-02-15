a535e69f469610886975262fa0f1e2197413dcfe
417-72KI <417.72ki@gmail.com>
2023-02-15 18:00:02 +0900

    pseudo overload function for `ghClient`

--- github.go
+++ github.go
@@ -10,8 +10,34 @@ import (
 	"golang.org/x/oauth2"
 )
 
-func ghClient(ctx context.Context) (*github.Client, error) {
+type clientOptions struct {
+	token   string
+	baseURL string
+}
+
+type clientOption func(*clientOptions)
+
+func withToken(token string) clientOption {
+	return func(ops *clientOptions) {
+		ops.token = token
+	}
+}
+
+func withBaseURL(baseURL string) clientOption {
+	return func(ops *clientOptions) {
+		ops.baseURL = baseURL
+	}
+}
+
+func ghClient(ctx context.Context, ops ...clientOption) (*github.Client, error) {
+	opts := clientOptions{}
+	for _, op := range ops {
+		op(&opts)
+	}
 	token := os.Getenv("GITHUB_TOKEN")
+	if opts.token != "" {
+		token = opts.token
+	}
 	if token == "" {
 		return nil, errors.New("github token is missing. please use GITHUB_TOKEN environment variable")
 	}
@@ -19,6 +45,9 @@ func ghClient(ctx context.Context) (*github.Client, error) {
 	tc := oauth2.NewClient(ctx, ts)
 	client := github.NewClient(tc)
 	baseURL := os.Getenv("GITHUB_BASE_URL")
+	if opts.baseURL != "" {
+		baseURL = opts.baseURL
+	}
 	if baseURL != "" {
 		var err error
 		client, err = github.NewEnterpriseClient(baseURL, baseURL, tc)
