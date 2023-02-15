f73a29a34d0fcf5b69c62e28f1d728610541b622
417-72KI <417.72ki@gmail.com>
2023-02-15 19:19:14 +0900

    view command

---
+++ cmd_view.go
@@ -0,0 +1,64 @@
+package milestone
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	"github.com/cli/cli/v2/pkg/cmdutil"
+	"github.com/spf13/cobra"
+)
+
+func newViewCmd(f *cmdutil.Factory) *cobra.Command {
+	var (
+		selector string
+		webMode  bool
+	)
+
+	io := f.IOStreams
+
+	viewCmd := &cobra.Command{
+		Use:   "view <number> [flags]",
+		Short: "Display the information about a milestone",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			selector = args[0]
+
+			ctx := context.Background()
+			io.DetectTerminalTheme()
+			if num, err := strconv.Atoi(selector); err == nil {
+				baseRepo, err := f.BaseRepo()
+				if err != nil {
+					return err
+				}
+				owner := baseRepo.RepoOwner()
+				repo := baseRepo.RepoName()
+				io.StartProgressIndicator()
+				milestone, err := getMilestone(ctx, owner, repo, num)
+				io.StopProgressIndicator()
+				if err != nil {
+					return err
+				}
+				if webMode {
+					milestoneURL := *milestone.HTMLURL
+					if err != nil {
+						return err
+					}
+					if f.IOStreams.IsStdoutTTY() {
+						fmt.Fprintf(f.IOStreams.ErrOut, "Opening %s in your browser.\n", milestoneURL)
+					}
+					f.Browser.Browse(milestoneURL)
+					return nil
+				}
+				if io.IsStdoutTTY() {
+					return printReadableMilestonePreview(io, milestone)
+				}
+				return printRawMilestonePreview(io.Out, milestone)
+			} else {
+				return err
+			}
+		},
+	}
+	viewCmd.Flags().BoolVarP(&webMode, "web", "w", false, "List milestones in the web browser")
+	return viewCmd
+}
--- display.go
+++ display.go
@@ -2,12 +2,14 @@ package milestone
 
 import (
 	"fmt"
+	"io"
 	"regexp"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/cli/cli/v2/pkg/iostreams"
+	"github.com/cli/cli/v2/pkg/markdown"
 	"github.com/cli/cli/v2/utils"
 	"github.com/cli/go-gh/pkg/text"
 	"github.com/google/go-github/v47/github"
@@ -57,10 +59,68 @@ func PrintMilestones(io *iostreams.IOStreams, now time.Time, prefix string, tota
 	}
 }
 
-func ColorForMilestoneState(cs *iostreams.ColorScheme, milestone *github.Milestone) func(string) string {
+func printRawMilestonePreview(out io.Writer, milestone *github.Milestone) error {
+	fmt.Fprintf(out, "title:\t\t%s\n", *milestone.Title)
+	fmt.Fprintf(out, "state:\t\t%s\n", *milestone.State)
+	fmt.Fprintf(out, "description:\t%s\n", *milestone.Description)
+	fmt.Fprintf(out, "due on:\t\t%s\n", *milestone.DueOn)
+	return nil
+}
+
+func printReadableMilestonePreview(io *iostreams.IOStreams, milestone *github.Milestone) error {
+	out := io.Out
+	cs := io.ColorScheme()
+	now := time.Now()
+
+	fmt.Fprintf(out, "%s (%d)\n", cs.Bold(*milestone.Title), *milestone.Number)
+	fmt.Fprintf(out,
+		"%s • Last updated %s\n",
+		milestoneStateWithColor(cs, now, milestone),
+		text.RelativeTimeAgo(now, *milestone.UpdatedAt),
+	)
+
+	var (
+		md  string
+		err error
+	)
+	if *milestone.Description == "" {
+		md = fmt.Sprintf("\n  %s\n\n", cs.Gray("No description provided"))
+	} else {
+		md, err = markdown.Render(*milestone.Description,
+			markdown.WithTheme(io.TerminalTheme()),
+			markdown.WithWrap(io.TerminalWidth()))
+		if err != nil {
+			return err
+		}
+	}
+	fmt.Fprintf(out, "\n%s\n", md)
+	fmt.Fprintf(out, cs.Gray("View this milestone on GitHub: %s\n"), *milestone.HTMLURL)
+
+	return nil
+}
+
+func milestoneStateWithColor(cs *iostreams.ColorScheme, now time.Time, milestone *github.Milestone) string {
+	if *milestone.State == "closed" {
+		return fmt.Sprintf("%s %s", cs.Bold("Closed"), text.RelativeTimeAgo(now, *milestone.ClosedAt))
+	}
+	dueDate := milestone.DueOn
+	if dueDate == nil {
+		return "No due date"
+	} else if now.Before(*dueDate) {
+		return fmt.Sprintf("Due by %s", dueDate.Format("January 02, 2006"))
+	} else {
+		return fmt.Sprintf("Past due by %s", text.RelativeTimeAgo(now, *dueDate))
+	}
+}
+
+func colorForMilestoneState(cs *iostreams.ColorScheme, now time.Time, milestone *github.Milestone) func(string) string {
 	switch *milestone.State {
 	case "open":
-		return cs.Green
+		if now.Before(*milestone.DueOn) {
+			return cs.Green
+		} else {
+			return cs.Yellow
+		}
 	case "closed":
 		return cs.Magenta
 	default:
