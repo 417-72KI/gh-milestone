12e20da170129bbca0806008386cd1dc1dc02dd7
417-72KI <417.72ki@gmail.com>
2023-08-06 02:43:52 +0900

    create viewOptions

--- milestone/cmd_view.go
+++ milestone/cmd_view.go
@@ -3,46 +3,55 @@ package milestone
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"strconv"
 
 	"github.com/417-72KI/gh-milestone/milestone/internal/api"
 	iMilestone "github.com/417-72KI/gh-milestone/milestone/internal/milestone"
 
 	"github.com/cli/cli/v2/pkg/cmdutil"
+	"github.com/cli/cli/v2/pkg/iostreams"
 	"github.com/spf13/cobra"
 )
 
-func newViewCmd(f *cmdutil.Factory) *cobra.Command {
-	var (
-		selector string
-		webMode  bool
-	)
+type viewOptions struct {
+	HttpClient func() (*http.Client, error)
+	IO         *iostreams.IOStreams
 
-	io := f.IOStreams
+	Selector string
+	WebMode  bool
+	Exporter cmdutil.Exporter
+}
+
+func newViewCmd(f *cmdutil.Factory) *cobra.Command {
+	opts := &viewOptions{
+		IO:         f.IOStreams,
+		HttpClient: f.HttpClient,
+	}
 
 	viewCmd := &cobra.Command{
 		Use:   "view <number> [flags]",
 		Short: "Display the information about a milestone",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			selector = args[0]
+			opts.Selector = args[0]
 
 			ctx := context.Background()
-			io.DetectTerminalTheme()
-			if num, err := strconv.Atoi(selector); err == nil {
+			opts.IO.DetectTerminalTheme()
+			if num, err := strconv.Atoi(opts.Selector); err == nil {
 				baseRepo, err := f.BaseRepo()
 				if err != nil {
 					return err
 				}
 				owner := baseRepo.RepoOwner()
 				repo := baseRepo.RepoName()
-				io.StartProgressIndicator()
+				opts.IO.StartProgressIndicator()
 				milestone, err := api.GetMilestone(ctx, owner, repo, num)
-				io.StopProgressIndicator()
+				opts.IO.StopProgressIndicator()
 				if err != nil {
 					return err
 				}
-				if webMode {
+				if opts.WebMode {
 					milestoneURL := *milestone.HTMLURL
 					if err != nil {
 						return err
@@ -53,15 +62,15 @@ func newViewCmd(f *cmdutil.Factory) *cobra.Command {
 					f.Browser.Browse(milestoneURL)
 					return nil
 				}
-				if io.IsStdoutTTY() {
-					return iMilestone.PrintReadableMilestonePreview(io, milestone)
+				if opts.IO.IsStdoutTTY() {
+					return iMilestone.PrintReadableMilestonePreview(opts.IO, milestone)
 				}
-				return iMilestone.PrintRawMilestonePreview(io.Out, milestone)
+				return iMilestone.PrintRawMilestonePreview(opts.IO.Out, milestone)
 			} else {
 				return err
 			}
 		},
 	}
-	viewCmd.Flags().BoolVarP(&webMode, "web", "w", false, "List milestones in the web browser")
+	viewCmd.Flags().BoolVarP(&opts.WebMode, "web", "w", false, "List milestones in the web browser")
 	return viewCmd
 }
